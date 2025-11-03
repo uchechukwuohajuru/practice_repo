7eb914b22a64e0d58e5670a35511a0aa3a79f3b4
uchechukwuohajuru <uchechukwuohajuru@gmail.com>
2025-11-03 13:09:01 +0100

    Adding a practice repo

---
+++ practice1.py
@@ -0,0 +1,13 @@
+# This is the question to be implemented in your code.
+
+'''Question 1.
+
+a. Fork this repo.
+b. Clone the repo on Vscode
+c. Initialize an upstream
+d. checkout a new branch name with your_name
+e. Write a python function called my_name that prints your name.
+f. push to the upstream on the branch your_name you created
+g. Do a pull request
+
+'''
